3f19f907efabaa84ead77b099a83c3968e5ad28d
Onur Aksoy <onur.aksoy.3445@gmail.com>
2023-08-08 18:17:12 +0300

    Hospital_test(2)

---
+++ oa_hospital2/models/appointment.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models
+
+
+class HospitalAppointment(models.Model):
+    _name = "hospital.appointment"
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+    _description = "Hospital Appointment"
+    _rec_name = 'ref'
+
+    patient_id = fields.Many2one(comodel_name='hospital.patient', string="Patient")
+    gender = fields.Selection(related='patient_id.gender')
+    appointment_time = fields.Datetime(string='Appointment Time', default=fields.Datetime.now)
+    booking_date = fields.Date(string='Booking Date', default=fields.Date.context_today)
+    ref = fields.Char(string='Reference', help="Reference of the patient from patient record.")
+    prescription = fields.Html(string='Prescription')
+    priority = fields.Selection([
+        ('0', 'Low'),
+        ('1', 'Normal'),
+        ('2', 'High'),
+        ('3', 'Very High')], string="Priority")
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('in_consultation', 'ın Consultation'),
+        ('done', 'Done'),
+        ('cancel', 'Cancelled')], default='draft', string="Status", required=True)
+
+    @api.onchange('patient_id')
+    def onchange_patient_id(self):
+        self.ref = self.patient_id.ref
+
+    def action_test(self):
+        return  {
+            'effect': {
+                'fadeout': 'slow',
+                'message': 'Here is the rainbow c:',
+                'type': 'rainbow_man',
+            }
+        }
