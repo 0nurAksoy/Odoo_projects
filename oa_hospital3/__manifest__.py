b52883c8f8806f08897045c923dc862355ca4eb4
Onur Aksoy <onur.aksoy.3445@gmail.com>
2023-08-16 17:48:39 +0300

    Test(3)

---
+++ oa_hospital3/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Hospital Management ',
+    'version': '1.0',
+    'category': 'Hospital',
+    'author': 'onur',
+    'sequence': -110,
+    'summary': 'Hospital Management System',
+    'description': """ Hospital Management System """,
+    'depends': ['mail', 'product'],
+    'data':[
+        'security/ir.model.access.csv',
+        'views/menu.xml',
+        'views/patient_view.xml',
+        'views/female_patient_view.xml',
+        'views/appointment_view.xml',
+    ],
+    'demo': [],
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
