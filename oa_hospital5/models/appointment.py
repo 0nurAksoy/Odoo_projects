cc0cc7c8668284f826a176ec70fe30805d3de8e5
Onur Aksoy <onur.aksoy.3445@gmail.com>
2023-08-31 14:12:56 +0300

    patient_id hatası (hepsini kaldırdım)

--- oa_hospital5/models/appointment.py
+++ oa_hospital5/models/appointment.py
@@ -7,13 +7,13 @@ class HospitalAppointment(models.Model):
     _name = "hospital.appointment"
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = "Hospital Appointment"
-    _rec_name = 'ref'
+    # _rec_name = 'ref'
 
-    patient_id = fields.Many2one(comodel_name='hospital.patient', string="Patient", ondelete='cascade')
-    gender = fields.Selection(related='patient_id.gender')
+    # patient_id = fields.Many2one(comodel_name='hospital.patient', string="Patient", ondelete='cascade')
+    # gender = fields.Selection(related='patient_id.gender')
     appointment_time = fields.Datetime(string='Appointment Time', default=fields.Datetime.now)
     booking_date = fields.Date(string='Booking Date', default=fields.Date.context_today)
-    ref = fields.Char(string='Reference', help="Reference of the patient from patient record.")
+    # ref = fields.Char(string='Reference', help="Reference of the patient from patient record.")
     prescription = fields.Html(string='Prescription')
     priority = fields.Selection([
         ('0', 'Low'),
@@ -30,10 +30,10 @@ class HospitalAppointment(models.Model):
     hide_sales_price = fields.Boolean(string="Hide Sales Price")
 
 
-    @api.model
-    def create(self, vals):
-        vals['ref'] = self.env['ir.sequence'].next_by_code('hospital.patient')
-        return super(HospitalPatient, self).create(vals)
+    # @api.model
+    # def create(self, vals):
+    #     vals['ref'] = self.env['ir.sequence'].next_by_code('hospital.patient')
+    #     return super(HospitalPatient, self).create(vals)
 
     # yukarıdaki yaptığımız tanımlamayla 'appointment' menüsünde yeni bir hasta kaydı oluşturup,
     # kaydettiğimizde,(create metodu için) 'ref' alanı otomatik olarak "OA------" olarak doldurulacak
