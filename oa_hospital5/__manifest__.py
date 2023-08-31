3f64b639797c6513e107dd042b094fc6ad256831
Onur Aksoy <onur.aksoy.3445@gmail.com>
2023-08-31 10:02:20 +0300

    Test (5)
    Eksikleri tamamlama

---
+++ oa_hospital5/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Hospital Management ',
+    'version': '1.0',
+    'category': 'Hospital',
+    'author': 'Onur Aksoy',
+    'sequence': -130,
+    'summary': 'Hospital Management System',
+    'description': """ Hospital Management System """,
+    'depends': ['mail', 'product'],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/patient_tag_data.xml',
+        'data/patient.tag.csv',
+        'data/sequence_data.xml',
+        'wizard/cancel_appointment_view.xml',
+        'views/menu.xml',
+        'views/patient_view.xml',
+        'views/female_patient_view.xml',
+        'views/appointment_view.xml',
+        'views/patient_tag_view.xml',
+        'views/odoo_playground_view.xml',
+        'views/res_config_settings_view.xml',
+
+    ],
+    'demo': [],
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
---
+++ oa_hospital5/models/patient.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+from datetime import date
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
+
+
+class HospitalPatient(models.Model):
+    _name = "hospital.patient"
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+    _description = "Hospital Patient"
+
+    name = fields.Char(string='Name', tracking=True)
+    date_of_birth = fields.Date(string='Date of Birth')
+    ref = fields.Char(string='Reference', tracking=True)
+    age = fields.Integer(string="Age", compute='_compute_age', tracking=True)
+    gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string='Gender', tracking=True)
+    active = fields.Boolean(string='Active', default=True)
+    appointment_id = fields.Many2one(comodel_name='hospital.appointment', string="Appointments")
+    image = fields.Image(string="Image")
+    tag_ids = fields.Many2many('patient.tag', string="Tags")
+    appointment_count = fields.Integer(string="Appointment Count", compute='_compute_appointment_count', store=True)
+    appointment_ids = fields.One2many('hospital.patient', 'patient_id', string="Appointments")
+    parent = fields.Char(string="Parent")
+    marital_status = fields.Selection([('married', 'Married'), ('single', 'Single'), ], string="Marital Status",
+                                      tracking=True)
+    partner_name = fields.Char(string="Partner Name")
+
+    @api.depends('appointment_ids')
+    def _compute_appointment_count(self):
+        for rec in self:
+            rec.appointment_count = self.env['hospital_appointment'].search_count([('patient_id', '=', rec.id)])
+
+    @api.constrains('date_of_birth')
+    def _check_date_of_birth(self):
+        for rec in self:
+            if rec.date_of_birth and rec.date_of_birth > fields.Date.today():
+                raise ValidationError(_("The entered date of birth is not acceptable"))
+
+    @api.ondelete(at_uninstall=False)
+    def check_appointments(self):
+        for rec in self:
+            if rec.appointment.ids:
+                raise ValidationError(_("You cannot delete a patient with appointments !"))
+
+    @api.model
+    def create(self, vals):
+        vals['ref'] = self.env['ir.sequence'].next_by_code('hospital.patient')
+        return super(HospitalPatient, self).create(vals)
+
+    def write(self, vals):
+        if not self.ref and not vals.get('ref'):
+            vals['ref'] = self.env['ir.sequence'].next_by_code('hospital.patient')
+        return super(HospitalPatient, self).write(vals)
+
+    # Patient menüsündeki, herhangi bir kayıt üzerinde değişiklik yapıldığında çalışıyor.
+
+    @api.depends('date_of_birth')
+    def _compute_age(self):
+        for rec in self:
+            today = date.today()
+            if rec.date_of_birth:
+                rec.age = today.year - rec.date_of_birth.year
+            else:
+                rec.age = 1
+
+    def name_get(self):
+        return [(record.id, "%s [%s]" % (record.name, record.ref)) for record in self]
+
+    def action_test(self):
+        return
