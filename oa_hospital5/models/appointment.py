55fcf055b6fbc74331dfc3fc5cc90f1e3f2bd14e
Onur Aksoy <onur.aksoy.3445@gmail.com>
2023-09-01 09:57:17 +0300

    Patient_id hatası düzeltme denemeleri

--- oa_hospital5/models/appointment.py
+++ oa_hospital5/models/appointment.py
@@ -7,10 +7,10 @@ class HospitalAppointment(models.Model):
     _name = "hospital.appointment"
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = "Hospital Appointment"
-    # _rec_name = 'ref'
+    _rec_name = 'ref'
 
-    # patient_id = fields.Many2one(comodel_name='hospital.patient', string="Patient", ondelete='cascade')
-    # gender = fields.Selection(related='patient_id.gender')
+    patient_id = fields.Many2one(comodel_name='hospital.patient', string="Patient", ondelete='cascade')
+    gender = fields.Selection(related='patient_id.gender')
     appointment_time = fields.Datetime(string='Appointment Time', default=fields.Datetime.now)
     booking_date = fields.Date(string='Booking Date', default=fields.Date.context_today)
     # ref = fields.Char(string='Reference', help="Reference of the patient from patient record.")
@@ -30,10 +30,10 @@ class HospitalAppointment(models.Model):
     hide_sales_price = fields.Boolean(string="Hide Sales Price")
 
 
-    # @api.model
-    # def create(self, vals):
-    #     vals['ref'] = self.env['ir.sequence'].next_by_code('hospital.patient')
-    #     return super(HospitalPatient, self).create(vals)
+    @api.model
+    def create(self, vals):
+        vals['ref'] = self.env['ir.sequence'].next_by_code('hospital.patient')
+        return super(HospitalPatient, self).create(vals)
 
     # yukarıdaki yaptığımız tanımlamayla 'appointment' menüsünde yeni bir hasta kaydı oluşturup,
     # kaydettiğimizde,(create metodu için) 'ref' alanı otomatik olarak "OA------" olarak doldurulacak
@@ -43,9 +43,9 @@ class HospitalAppointment(models.Model):
             raise ValidationError(_("You can delete appointment only in Draft status!"))
         return super(HospitalAppointment).unlink()
 
-    # @api.onchange('patient_id')
-    # def onchange_patient_id(self):
-    #     self.ref = self.patient_id.ref
+    @api.onchange('patient_id')
+    def onchange_patient_id(self):
+        self.ref = self.patient_id.ref
 
     def action_test(self):
         return {
